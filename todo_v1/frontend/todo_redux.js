bb6f63206b60960acd507d15ccbaf16c319dd6fb
tjohnny93 <tjohnny93@berkeley.edu>
2019-06-24 14:33:33 -0700

    Implement todos reducer and connect to redux store

--- todo_v1/frontend/todo_redux.js
+++ todo_v1/frontend/todo_redux.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+const store = configureStore();
+window.store = store;
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <h2>React is working!!</h2>,
