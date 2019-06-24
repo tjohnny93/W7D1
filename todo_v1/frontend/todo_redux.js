4be35f17208bca118b2f6aed4eec2ec900de63b9
tjohnny93 <tjohnny93@berkeley.edu>
2019-06-24 14:07:02 -0700

    Complete react/redux config w/ webpack/babel

---
+++ todo_v1/frontend/todo_redux.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <h2>React is working!!</h2>,
+    document.getElementById('root')
+  );
+});
