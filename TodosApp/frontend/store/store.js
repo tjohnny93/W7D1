e35c42c9024da9c0108bcb52a35f0eef6fd2dd19
tjohnny93 <tjohnny93@berkeley.edu>
2019-06-25 15:09:03 -0700

    Create Thunk Middleware(working)

--- TodosApp/frontend/store/store.js
+++ TodosApp/frontend/store/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/root_reducer";
 import logger from 'redux-logger';
+import thunk from '../middleware/thunk.js';
 
 const configureStore = (preloadedState = {}) => {
-  return createStore(rootReducer, preloadedState, applyMiddleware(logger));
+  return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
 };
 
 export default configureStore;
